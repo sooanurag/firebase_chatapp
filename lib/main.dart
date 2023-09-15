3d16904fb2ba131f0be072f05a3ec482822aa616
Anurag Gupta <anuballia01@gmail.com>
2023-09-16 00:10:50 +0530

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_chatapp/Firebase/firebase_options.dart';
 import 'package:firebase_chatapp/Models/firebaseHelper.dart';
-import 'package:firebase_chatapp/Models/user_model.dart';
-import 'package:firebase_chatapp/Screens/complete_profile.dart';
 import 'package:firebase_chatapp/Screens/home_page.dart';
 import 'package:firebase_chatapp/Screens/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
